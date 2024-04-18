d7a31e263c5391a141db006bf5c7b6b9945fa504
Eduardo Canchola <canchola700@gmail.com>
2024-04-18 16:05:24 -0600

    App terminada

---
+++ src/presentation/components/shared/PrimaryButton.tsx
@@ -0,0 +1,37 @@
+
+import React from 'react'
+import { Platform, Pressable, StyleSheet, Text } from 'react-native'
+
+interface Props {
+  label: string;
+  onPress?: () => void;
+  onLongPress?: () => void;
+}
+
+export const PrimaryButton = ({label, onPress, onLongPress}: Props) => {
+  return (
+    <Pressable 
+        style={ ({pressed}) => [
+          styles.button,
+          pressed && styles.buttonPress
+        ] }
+        onPress={ () => onPress && onPress() }
+        onLongPress={ () => onLongPress && onLongPress() }
+      >
+        <Text style={{ color: Platform.OS === 'android' ? 'white' : '#4746AB'}}>
+        { label }</Text>
+      </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor:  Platform.OS === 'android' ? '#5856D6' : 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10
+  },
+  buttonPress: {
+    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white'
+  },
+})
